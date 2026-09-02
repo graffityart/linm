876fab2aa5b89802e97b2449f9eb3a41b6b74ab2
조문국 <120075237+graffityart@users.noreply.github.com>
2026-09-02 14:19:36 +0900

    Add business and contact information

---
+++ data/site.js
@@ -0,0 +1,13 @@
+export const siteInfo = {
+  brand: '몽땅 다이아',
+  advertisingName: '몽땅 다이아',
+  representative: '김상훈',
+  address: '대구광역시 서구 국채보상로 176 5층',
+  phone: '010-6289-3810',
+  kakaoId: 'pay3810',
+  kakaoUrl: 'http://qr.kakao.com/talk/i2upgpbu1am6frQeVqvvHamKNWg-',
+  businessNumber: '503-91-97230',
+  ecommerceReport: '제2011-대구서구-0017호',
+  privacyOfficer: '김상훈',
+  email: 'buyman1@naver.com',
+};
