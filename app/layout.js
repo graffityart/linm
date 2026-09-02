64591970c173060786ed46610c6258c066184d5e
조문국 <120075237+graffityart@users.noreply.github.com>
2026-09-02 17:22:19 +0900

    Load responsive uploaded image styles

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import './image-styles.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import FloatingContact from '../components/FloatingContact';
