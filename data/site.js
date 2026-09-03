e47d2645ccacdb71fd51384ba331a33be67926e9
조문국 <120075237+graffityart@users.noreply.github.com>
2026-09-03 13:48:23 +0900

    Update GV Pay business information

--- data/site.js
+++ data/site.js
@@ -1,13 +1,12 @@
 export const siteInfo = {
-  brand: '몽땅 다이아',
+  brand: '지브이페이(GV페이)',
   advertisingName: '몽땅 다이아',
-  representative: '김상훈',
-  address: '대구광역시 서구 국채보상로 176 5층',
+  representative: '신용황',
+  address: '대구광역시 서구 북비산로 382-2 (비산동)',
   phone: '010-6289-3810',
   kakaoId: 'pay3810',
   kakaoUrl: 'http://qr.kakao.com/talk/i2upgpbu1am6frQeVqvvHamKNWg-',
-  businessNumber: '503-91-97230',
-  ecommerceReport: '제2011-대구서구-0017호',
-  privacyOfficer: '김상훈',
+  businessNumber: '798-96-01263',
+  privacyOfficer: '신용황',
   email: 'buyman1@naver.com',
 };
